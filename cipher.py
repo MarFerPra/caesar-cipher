5f24428d3cef761730785ea9221712cdad09ecb0
MarFerPra <marco.fp94@gmail.com>
2015-09-28 11:21:17 +0200

    01

---
+++ cipher.py
@@ -0,0 +1,37 @@
+# Written following "Hacking Secret Ciphers with Python" by Al Sweigart
+# https://inventwithpython.com/hacking/
+# With the only purpouse of getting in touch with Python and procrastinate during a boring class.
+
+mode = input('Encrypt or Decrypt? [E/D] \n')
+if mode == "E":
+    print("Encrypt-ing.\n")
+elif mode == "D":
+    print("Decrypt-ing.\n")
+
+message = input('Enter message: \n')
+
+key = input('Enter key: [1-25] \n')
+print('Using key: '+key+'\n')
+key = int(key)
+
+
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+message = message.upper()
+result = ""
+
+for char in message:
+    if char in LETTERS:
+        x = LETTERS.find(char)
+        if(mode == 'E'):
+            x = x+key
+        elif(mode == 'D'):
+            x = x-key
+
+        if x >= 26:
+            x = x-26
+        elif x < 0:
+            x = x+26
+
+        result = result + LETTERS[x]
+
+print('Result: '+result)
